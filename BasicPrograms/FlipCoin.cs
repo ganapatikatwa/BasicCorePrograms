bda038c7e6b09d74f5bfb7af61af5e66d9507fd7
Ganapati <ganapatasakatwa@gmail.com>
2023-05-21 11:56:20 +0530

    [GK] Flip Coin and Print Percentage of Head and Tails

---
+++ BasicPrograms/FlipCoin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class FlipCoin
+    {
+        public void HeadTails()
+        {
+            int i;
+            int headcount = 0;
+            int tailscount = 0;
+            //Console.WriteLine("Flip Coin and print percentage of Heads and Tails");
+            Console.WriteLine("Enter the Number of time You want to Flip The Coin");
+            int NumCount = Convert.ToInt32(Console.ReadLine());
+
+            for(i=1; i<=NumCount; i++)
+            {
+                Random random = new Random();
+                int randomheadtails = random.Next(0,1);
+                if(randomheadtails<1)
+                {
+                    tailscount++;
+                }
+                else
+                {
+                    headcount++;
+                }
+            }
+            Console.WriteLine("Percentage of Head and Tails");
+             float perhead = headcount / NumCount * 100;
+            Console.WriteLine("Percentage of Head Count is --"+perhead);
+            float pertails = tailscount / NumCount * 100;
+            Console.WriteLine("Percentage of Tails Count is --" + pertails);
+        }
+    }
+}
---
+++ BasicPrograms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasicPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Wel Come to Basic Core Programs");
+            while(true)
+            {
+                Console.WriteLine("Enter the Choice \n 1)Filp Coin");
+                int choice= Convert.ToInt32(Console.ReadLine());
+                switch(choice)
+                {
+                    case 1:
+                        FlipCoin flipcoin= new FlipCoin();
+                        flipcoin.HeadTails();
+                        break;
+                }
+            }
+        }
+    }
+}
