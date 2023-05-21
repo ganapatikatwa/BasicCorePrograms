948b83f7cd6956185e63b5500b8a08a9d6cad709
Ganapati <ganapatasakatwa@gmail.com>
2023-05-21 20:19:37 +0530

    [GK] Power of 2 Calculating

---
+++ BasicPrograms/PowerOf2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPrograms
+{
+    public class PowerOf2
+    {
+        public void pow2()
+        {
+            Console.WriteLine("Enter the Number to Know the Power of 2");
+            int power=Convert.ToInt32(Console.ReadLine());
+            double Result = 0;
+            int i;
+            
+            for (i=1;i<=power;i++)
+            {
+                Result = Math.Pow(2, power);
+                Console.WriteLine($"2^{i} = {Result}");
+            }
+            Console.WriteLine("The Power of 2 is--"+Result);
+        }
+    }
+}
--- BasicPrograms/Program.cs
+++ BasicPrograms/Program.cs
@@ -9,18 +9,29 @@ namespace BasicPrograms
             Console.WriteLine("Wel Come to Basic Core Programs");
             while(true)
             {
-                Console.WriteLine("Enter the Choice \n 1)Filp Coin\n2)Leap Year");
+                Console.WriteLine("Enter the Choice \n 1)Filp Coin\n2)Leap Year\n3)Power Of 2");
                 int choice= Convert.ToInt32(Console.ReadLine());
-                switch(choice)
+                if (choice != 0)
                 {
-                    case 1:
-                        FlipCoin flipcoin= new FlipCoin();
-                        flipcoin.HeadTails();
-                        break;
-                    case 2:
-                        LeapYear leapYear= new LeapYear();
-                        leapYear.Year();
-                        break;
+                    switch (choice)
+                    {
+                        case 1:
+                            FlipCoin flipcoin = new FlipCoin();
+                            flipcoin.HeadTails();
+                            break;
+                        case 2:
+                            LeapYear leapYear = new LeapYear();
+                            leapYear.Year();
+                            break;
+                        case 3:
+                            PowerOf2 powerOf2 = new PowerOf2();
+                            powerOf2.pow2();
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter the Correct Choice");
                 }
             }
         }
